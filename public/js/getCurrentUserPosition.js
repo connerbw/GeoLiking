4dcacf0bc2bf153cfad2d6f65aaf91db8cca286b
Dac Chartrand <dac.chartrand@gmail.com>
2014-08-07 23:25:27 -0400

    Fix Firefox scope bug

--- public/js/getCurrentUserPosition.js
+++ public/js/getCurrentUserPosition.js
@@ -1,24 +1,24 @@
 (function () {
 
+    function success(position) {
+        var pos = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+        $.cookie('lastKnownUserPos', JSON.stringify(pos), { expires: 1, path: '/' });
+    }
+
+    function error(error) {
+        $.removeCookie('lastKnownUserPos', { path: '/' });
+        console.warn('ERROR(' + error.code + '): ' + error.message);
+    }
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
             success,
             error,
             { enableHighAccuracy: true, maximumAge: 60000 }
         );
-
-        function success(position) {
-            var pos = {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude
-            };
-            $.cookie('lastKnownUserPos', JSON.stringify(pos), { expires: 1, path: '/' });
-        }
-
-        function error(error) {
-            $.removeCookie('lastKnownUserPos', { path: '/' });
-            console.warn('ERROR(' + error.code + '): ' + error.message);
-        }
     }
 
 }());
