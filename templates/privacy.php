fb904fc5e6eb4b773f2269851e47b58c1f827961
Dac Chartrand <dac.chartrand@gmail.com>
2015-06-23 14:39:57 -0400

    Privacy page formatting

--- templates/privacy.php
+++ templates/privacy.php
@@ -17,15 +17,19 @@
 </head>
 <body>
 
-<h2>Privacy Policy</h2>
-<p>At GeoLiking.com, we recognize that the privacy of your personal information is important. What follows is information about the data received and/or collected when you visit GeoLiking.com.</p>
-<p><strong>Log Files</strong></p>
-<p>Like every other website, we have log files. The information in the log files include your <a href="http://en.wikipedia.org/wiki/IP_address">IP address</a>, the browser you used to visit our site, the time you visited our site, and which pages you visited throughout our site.</p>
-<p><strong>Cookies, Web Beacons</strong></p>
-<p>We use <a href="http://en.wikipedia.org/wiki/HTTP_cookie">cookies</a> to store information, such as your personal preferences when you visit our site.</p>
-<p>Third party vendors such as Google, Facebook, Etc. may use cookies and/or <a href="http://en.wikipedia.org/wiki/Web_bug">web beacons</a> to provide services on GeoLiking.com</p>
-<p>GeoLiking.com has no access or control over the technologies used by our third-party vendors.</p>
-<p>Thank you, the management.</p>
+<div class="row">
+    <div class="large-12 columns">
+    <h2>Privacy Policy</h2>
+    <p>At GeoLiking.com, we recognize that the privacy of your personal information is important. What follows is information about the data received and/or collected when you visit GeoLiking.com.</p>
+    <p><strong>Log Files</strong></p>
+    <p>Like every other website, we have log files. The information in the log files include your <a href="http://en.wikipedia.org/wiki/IP_address">IP address</a>, the browser you used to visit our site, the time you visited our site, and which pages you visited throughout our site.</p>
+    <p><strong>Cookies, Web Beacons</strong></p>
+    <p>We use <a href="http://en.wikipedia.org/wiki/HTTP_cookie">cookies</a> to store information, such as your personal preferences when you visit our site.</p>
+    <p>Third party vendors such as Google, Facebook, Etc. may use cookies and/or <a href="http://en.wikipedia.org/wiki/Web_bug">web beacons</a> to provide services on GeoLiking.com</p>
+    <p>GeoLiking.com has no access or control over the technologies used by our third-party vendors.</p>
+    <p>Thank you, the management.</p>
+    </div>
+</div>
 
 <div id="fb-root"></div>
 
