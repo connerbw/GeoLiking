514fb59ac387346b08a0ea10bf62ff71de41f544
Dac Chartrand <dac.chartrand@gmail.com>
2015-06-23 21:22:19 -0400

    Final touches

--- templates/position.php
+++ templates/position.php
@@ -1,6 +1,8 @@
 <?php
 /** @var \Slim\View $this */
 
+$geoLikingText = "I liked {$lat}, {$lng}";
+
 ?>
 <!DOCTYPE html>
 <!--[if IE 9]>
@@ -9,11 +11,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Geo Liking - Position</title>
+    <title>Geo Liking</title>
     <link rel="stylesheet" href="minify.php/css">
     <script src="minify.php/js-head"></script>
     <meta property="og:image" content="<?php echo $GLOBALS['CONFIG']['DOMAIN'] . $GLOBALS['CONFIG']['URL']; ?>img/GeolikingPlain.jpg" />
-    <meta name="description" content="Attach a novelty sized foam Facebook thumb to a selfie stick and like stuff. Post Geolocation coordinates to your wall. All the cool people are doing it!" />
+    <meta name="description" content="<?php echo $geoLikingText; ?>" />
 </head>
 <body>
 
@@ -31,12 +33,10 @@
     </div>
 
     <div class="large-4 columns">
-        <p><i>"I liked <?php echo "$lat, $lng"; ?>"</i></p>
-        <p><a href="<?php echo $GLOBALS['CONFIG']['URL']; ?>" class="button secondary">Back to homepage</a></p>
+        <p><i>"<?php echo $geoLikingText; ?>"</i></p>
+        <p><a href="<?php echo $GLOBALS['CONFIG']['URL']; ?>" class="button secondary">Go to homepage</a></p>
     </div>
 </div>
-
-
 
 <div class="row">
     <div class="large-12 columns">
